8d50adc734fae3c862f55ee968479d6a706b3f2b
Veaceslav <aramexfrontend@mail.ru>
2020-01-27 21:29:30 +0200

    bigimage

--- src/js/code.js
+++ src/js/code.js
@@ -5,12 +5,40 @@ function open(){
 		openmenu.addEventListener('click',function(){
 		const slideRight = document.querySelector('.header__navmobile');
 		const blur = document.querySelector('.main__section');
-
-		 slideRight.classList.add('slideToRight');
-		 blur.classList.add('blur');
+		document.querySelector('.open').classList.remove('show');
+		openmenu.classList.add('hide');
+		slideRight.classList.add('slideToRight');
+		blur.classList.add('blur');
+		
 	});
+	
 };
 
-open();
-
 //close bnt
+
+const close = () =>{
+	const closemenu = document.querySelector('.close');
+	closemenu.addEventListener('click',function(){
+		const slideRight = document.querySelector('.header__navmobile');
+		const blur = document.querySelector('.main__section');
+		document.querySelector('.open').classList.remove('hide');
+		document.querySelector('.open').classList.add('show');
+
+		slideRight.classList.remove('slideToRight');
+		blur.classList.remove('blur');
+	});
+}
+
+const bigimage= (eventObj) => {
+	const image = document.getElementsByTagName('img');
+
+
+	console.log(image);
+}
+
+
+
+
+open();
+close();
+bigimage();
