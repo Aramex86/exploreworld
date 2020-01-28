32031173adca836fe22757b602fa7fb3b5f20b85
Veaceslav <aramexfrontend@mail.ru>
2020-01-28 20:42:25 +0200

    bigimage animation

--- src/js/code.js
+++ src/js/code.js
@@ -1,24 +1,22 @@
-
-
-function open(){
+function open() {
 	const openmenu = document.querySelector('.open');
-		openmenu.addEventListener('click',function(){
+	openmenu.addEventListener('click', function () {
 		const slideRight = document.querySelector('.header__navmobile');
 		const blur = document.querySelector('.main__section');
 		document.querySelector('.open').classList.remove('show');
 		openmenu.classList.add('hide');
 		slideRight.classList.add('slideToRight');
 		blur.classList.add('blur');
-		
+
 	});
-	
+
 };
 
 //close bnt
 
-const close = () =>{
+const close = () => {
 	const closemenu = document.querySelector('.close');
-	closemenu.addEventListener('click',function(){
+	closemenu.addEventListener('click', function () {
 		const slideRight = document.querySelector('.header__navmobile');
 		const blur = document.querySelector('.main__section');
 		document.querySelector('.open').classList.remove('hide');
@@ -29,7 +27,7 @@ const close = () =>{
 	});
 }
 
-const bigimage= () => {
+const bigimage = () => {
 	//select all img
 	const image = document.getElementsByTagName('img');
 
@@ -37,27 +35,31 @@ const bigimage= () => {
 
 	imgarr.push(image);
 
-   	const imageselect = imgarr.map(function(index){
-		   for(let i =0; i< index.length; i++){
-			 
-			index[i].addEventListener('click',function(){
-				if(index[i] === index[0]){
+	const imageselect = imgarr.map(function (index) {
+		for (let i = 0; i < index.length; i++) {
+
+			index[i].addEventListener('click', function () {
+				if (index[i] === index[0]) {
 					const indonesia = document.querySelector('.indonesia__wrapp');
 					indonesia.classList.add('resizeimg');
+					document.querySelector('.back-btn').classList.add('moveright');
+					document.querySelector('.location__point').classList.add('moveleft');
+					document.querySelector('.page__intro-first').classList.add('movetop1');
+					document.querySelector('.page__intro-second').classList.add('movetop2');
+					document.querySelector('.page__nav').classList.add('moveleft');
 					indonesia.style.opacity = 1;
-					console.log(indonesia);
 				}
-			 });
-			
-			
-		   }
-			
-	   });
-	
-	
+			});
 
 
-	
+		}
+
+	});
+
+
+
+
+
 
 
 }
@@ -68,4 +70,3 @@ const bigimage= () => {
 open();
 close();
 bigimage();
-
